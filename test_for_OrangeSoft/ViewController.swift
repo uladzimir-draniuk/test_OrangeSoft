1a855d73ad6726a9577cf2d5770dfb74b55fad98
uladzimir.draniuk <uladzimir.draniuk@gmail.com>
2021-09-19 15:48:52 +0300

    Initial Commit

---
+++ test_for_OrangeSoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_for_OrangeSoft
+//
+//  Created by elf on 19.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
